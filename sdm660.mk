051406a6c0393aa475b70f63b6484bf22f30b0d1
SonicBSV <sonicbsv@ya.ru>
2024-11-29 22:28:21 +0300

    sdm660-common: drop DeviceSettings

--- sdm660.mk
+++ sdm660.mk
@@ -563,7 +563,3 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(COMMON_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(COMMON_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
-
-# DeviceSettings
-PRODUCT_PACKAGES += \
-   DeviceSettings

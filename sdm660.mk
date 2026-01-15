7505cefcef2c0d474d7128caa586cfd3d5544acc
SonicBSV <sonicbsv@ya.ru>
2026-01-15 07:32:25 +0300

    sdm660-common: added powerhint.xml

--- sdm660.mk
+++ sdm660.mk
@@ -409,7 +409,8 @@ PRODUCT_PACKAGES += \
    vendor.qti.hardware.perf@2.2.vendor
     
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf    
+    $(COMMON_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf \
+    $(COMMON_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml    
 
 # Public Libraries
 PRODUCT_COPY_FILES += \

781d842cf674c9cf66dc9111a12dbc5277227448
SonicBSV <sonicbsv@ya.ru>
2026-03-22 19:38:25 +0300

    sdm660-common: fix PRODUCT_AAPT_PREF_CONFIG value for X00TD/X01BD displays

--- sdm660.mk
+++ sdm660.mk
@@ -465,7 +465,7 @@ PRODUCT_PACKAGES += \
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG ?= xhdpi
+PRODUCT_AAPT_PREF_CONFIG ?= xxhdpi
 
 # Seccomp
 PRODUCT_COPY_FILES += \

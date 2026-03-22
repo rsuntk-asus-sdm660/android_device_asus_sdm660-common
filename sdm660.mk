c5371fe0e74011088a5b668ed061e1df7a94fe65
SonicBSV <sonicbsv@ya.ru>
2026-03-22 04:56:43 +0300

    sdm660-common: camera: switch *.mk to *.bp and android.hardware.camera.provider-service.lineage

--- sdm660.mk
+++ sdm660.mk
@@ -90,22 +90,15 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    camera.sdm660 \
-    libgui_vendor \
-    Aperture
-
-PRODUCT_PACKAGES += \
-    android.hardware.camera.common@1.0 \
-    android.hardware.camera.device@3.5 \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.provider@2.6 \
-    vendor.qti.hardware.camera.device@1.0 \
+    android.hardware.camera.provider-service_32.lineage \
     vendor.qti.hardware.camera.device@1.0.vendor \
+    camera.sdm660:32 \
+    libgui_vendor \
     liblz4.vendor \
     libutilscallstack.vendor \
     libpng.vendor:32 \
-    libxml2
+    libxml2 \
+    Aperture
 
 # Configstore
 PRODUCT_PACKAGES += \

9b6233c79484c279e8138845c03d34d5c5546c8b
SonicBSV <sonicbsv@ya.ru>
2026-05-28 18:30:36 +0300

    sdm660-common: optimize config.fs

--- config.fs
+++ config.fs
@@ -44,7 +44,7 @@ caps: BLOCK_SUSPEND NET_ADMIN
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
-caps: NET_BIND_SERVICE
+caps: NET_BIND_SERVICE SYS_BOOT
 
 [vendor/bin/pd-mapper]
 mode: 0755
@@ -110,28 +110,4 @@ caps: NET_BIND_SERVICE
 mode: 0771
 user: AID_SYSTEM
 group: AID_SYSTEM
-caps: 0
-
-[vendor/bt_firmware/]
-mode: 0771
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: 0
-
-[persist/]
-mode: 0771
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: 0
-
-[/mnt/vendor/persist/]
-mode: 0771
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: 0
-
-[vendor/dsp/]
-mode: 0771
-user: AID_MEDIA
-group: AID_MEDIA
 caps: 0

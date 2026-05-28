497f43f32b3728f20a86d7aed2e6ef004eb3962b
SonicBSV <sonicbsv@ya.ru>
2026-05-28 18:26:29 +0300

    sdm660-common: configs: add custom cgroups & task_profiles

--- sdm660.mk
+++ sdm660.mk
@@ -167,8 +167,8 @@ PRODUCT_PACKAGES += \
 # ============================================================
 
 PRODUCT_COPY_FILES += \
-    system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
-    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+    device/asus/sdm660-common/configs/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    device/asus/sdm660-common/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 
 # ============================================================

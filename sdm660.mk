6327989874d49a38fd17c8ce8269b218ca975bcc
SonicBSV <sonicbsv@ya.ru>
2026-05-28 18:41:56 +0300

    sdm660-common: add LiveDisplay

--- sdm660.mk
+++ sdm660.mk
@@ -403,6 +403,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
+# ============================================================
+# LiveDisplay native
+# ============================================================
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay-service.sdm
+
+$(call soong_config_set_bool,livedisplay_sdm,enable_dm,false)
+
 
 # ============================================================
 # Медиа — кодеки и профили

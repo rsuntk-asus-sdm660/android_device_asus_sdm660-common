18b0f3166d4dbfa442e4a16e8347ac993de69a9a
rsuntk <rissu.ntk@gmail.com>
2026-08-15 19:00:15 +0700

    sdm660-common: Enable blur support
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- common.mk
+++ common.mk
@@ -132,6 +132,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/displayconfig/display_id_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_0.xml
 
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.surface_flinger.supports_background_blur=1
+
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.egl=adreno \
     ro.hardware.vulkan=adreno \
